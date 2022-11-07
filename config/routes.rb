eceb1e9169bfc935ca01e6b32f3c271ed0a88179
David Lavilla <kngsks@DESKTOP-LAM01PN.localdomain>
2022-11-07 20:31:31 +0800

    configured users controller for added params in devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'user/sessions',
+    registrations: 'user/registrations'
+  }
+  
   get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
