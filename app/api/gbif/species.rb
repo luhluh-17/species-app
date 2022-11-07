63de4e248d79c3878f27700c7d66ae10709e371d
luhluh-17 <castro.ralph17@gmail.com>
2022-11-07 19:20:05 +0800

    add initial external api

---
+++ app/api/gbif/species.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Gbif
+  class Species
+    def self.all
+      Client.call(http_method: 'get', endpoint: 'species')
+    end
+
+    def self.search(name)
+      Client.call(http_method: 'get', endpoint: 'species/search', args: { name: })
+    end
+  end
+end
