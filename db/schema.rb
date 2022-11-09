4e2ce22b943c2c48dbac6652609a74effdddef62
David Lavilla <kngsks@DESKTOP-LAM01PN.localdomain>
2022-11-09 19:33:22 +0800

    integrated tailwind css

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_11_07_121652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "species", force: :cascade do |t|
+    t.string "genus"
+    t.string "ephitet"
+    t.string "name"
+    t.string "location"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,17 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_121652) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-
-  create_table "species", force: :cascade do |t|
-    t.string "genus"
-    t.string "ephitet"
-    t.string "name"
-    t.string "location"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   end
 
 end
