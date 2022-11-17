cf2a0c9333d46a2d3cd7853a8103980789329b62
luhluh-17 <castro.ralph17@gmail.com>
2022-11-17 13:32:33 +0800

    add initial sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def sidebar_item_style
+    'flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-red-100 dark:hover:bg-red-700'
+  end
 end
