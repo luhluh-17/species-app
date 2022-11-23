ff5fd001f3832fcb60fb6f7303c84b128009545f
David Lavilla <kngsks@DESKTOP-LAM01PN.localdomain>
2022-11-23 17:45:51 +0800

    refactored sidebar partial with cleaner code; deleted obsolete partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def sidebar_item_style
-    'flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-red-700'
+    'flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-red-700'
   end
 
   def classification_formatter(*args)
