a007d970433539b0ec38f7fb97951863f525f32e
David Lavilla <kngsks@DESKTOP-LAM01PN.localdomain>
2022-11-25 16:56:15 +0800

    added species validation, styled error message

--- app/models/species.rb
+++ app/models/species.rb
@@ -2,4 +2,6 @@ class Species < ApplicationRecord
   has_many :publications
   has_many :galleries
   accepts_nested_attributes_for :galleries, :publications
+
+  validates_presence_of :scientific_name
 end
